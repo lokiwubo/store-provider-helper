fdaf1999bff6feafd1e825279c04863fcc0c7ad4
loki <wb31202239@163.com>
2024-12-15 22:09:51 +0800

    fix

--- packages/react/eslint.config.mjs
+++ packages/react/eslint.config.mjs
@@ -2,6 +2,8 @@ import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 import globals from "globals";
 import tsEslint from "typescript-eslint";
+import reactEslintHooks from "eslint-plugin-react-hooks";
+import reactEslint from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -10,4 +12,6 @@ export default [
   pluginJs.configs.recommended,
   ...tsEslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-];
+  reactEslintHooks.configs.recommended,
+  reactEslint.configs.recommended,
+  ];
